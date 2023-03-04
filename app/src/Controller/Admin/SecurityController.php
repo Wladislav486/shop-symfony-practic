d8d43b8aa3904d871a2a5aa2ccc831e2416a343a
Wlad <vladgorbunov486@gmail.com>
2023-03-04 12:32:34 +0500

    031 Проверка на роль ROLE_ADMIN

--- app/src/Controller/Admin/SecurityController.php
+++ app/src/Controller/Admin/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -32,8 +33,8 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="admin_security_logout")
      */
-    public function logout(): void
+    public function logout(): RedirectResponse
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        return $this->redirectToRoute('admin_security_login');
     }
 }

e10d6701fc347ba446fa076f8d9aeda86397090d
Wlad <vladgorbunov486@gmail.com>
2023-03-06 21:12:22 +0500

    037 Страница всех продуктов

---
+++ app/src/Controller/Admin/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Form\EditProductFormType;
+use App\Repository\ProductRepository;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/product", name="admin_product_")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(ProductRepository $productRepository): Response
+    {
+
+        $products = $productRepository->findBy(['isDeleted' => false], ['id' => 'DESC'], 50);
+        return $this->render('admin/product/list.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @Route("/add", name="add")
+     */
+    public function edit(Request $request, Product $product = null): Response
+    {
+        $form = $this->createForm(EditProductFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+            return $this->redirectToRoute();
+        }
+    }
+
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(): Response
+    {
+
+    }
+
+}

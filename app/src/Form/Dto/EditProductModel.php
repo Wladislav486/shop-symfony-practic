ef226c0ff810923cab737f3e2441b69e5b1f1743
Wlad <gorbunov.v@intaro.email>
2023-11-07 00:03:46 +0500

    057 Валидации модели

--- app/src/Form/Dto/EditProductModel.php
+++ app/src/Form/Dto/EditProductModel.php
@@ -4,6 +4,7 @@ namespace App\Form\Dto;
 
 use App\Entity\Product;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class EditProductModel
 {
@@ -13,21 +14,30 @@ class EditProductModel
     public int $id;
 
     /**
+     * @Assert\NotBlank(message="Please enter a title")
      * @var string
      */
     public string $title;
 
     /**
+     * @Assert\NotBlank(message="Please enter a price")
+     * @Assert\GreaterThanOrEqual(value="0")
      * @var string
      */
     public string $price;
 
     /**
+     * @Assert\File(
+     *     maxSize = "5024k",
+     *     mimeTypes = {"image/jpeg", "image/png"},
+     *     mimeTypesMessage = "Please upload a valid image"
+     * )
      * @var UploadedFile|null
      */
     public ?UploadedFile $newImage;
 
     /**
+     * @Assert\NotBlank(message="Please indicate the quantity")
      * @var int
      */
     public int $quantity;

41121b805e129b813a62a0f05301e5f3b3501a84
vladgorbunov486 <vladgorbunov486@gmail.com>
2023-11-12 20:37:38 +0500

    074 Сохранение корзины

--- app/src/Controller/Main/CartController.php
+++ app/src/Controller/Main/CartController.php
@@ -3,31 +3,19 @@
 namespace App\Controller\Main;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
-/**
- * @Route("/api", name="main_api_")
- */
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", methods = "POST", name="cart_save")
+     * @Route("/cart", name="main_cart_show")
      */
-    public function saveCart(Request $request): Response
+    public function show(): Response
     {
 
-        $productId = $request->request->get('productId');
-        $phpSessionId = $request->cookies->get('PHPSESSID');
-
-        dd($productId, $phpSessionId);
-        return new JsonResponse([
-            'success' => false,
-            'data' => [
-                'test' => 123
-            ]
+        return $this->render('main/cart/show.html.twig', [
+            'controller_name' => 'CartController',
         ]);
     }
 }

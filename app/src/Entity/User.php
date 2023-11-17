bebcf1cada653fea0ca7cb62978b3d6a0987e9ad
vladgorbunov486 <vladgorbunov486@gmail.com>
2023-11-18 03:15:22 +0500

    077 Сущности Order и OrderProduct

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -67,10 +69,16 @@ class User implements UserInterface
      */
     private $isDeleted;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Order::class, mappedBy="owner")
+     */
+    private $orders;
+
 
     public function __construct()
     {
         $this->isDeleted = false;
+        $this->orders = new ArrayCollection();
     }
 
 
@@ -224,6 +232,36 @@ class User implements UserInterface
     public function setIsDeleted(?bool $isDeleted): self
     {
         $this->isDeleted = $isDeleted;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Order>
+     */
+    public function getOrders(): Collection
+    {
+        return $this->orders;
+    }
+
+    public function addOrder(Order $order): self
+    {
+        if (!$this->orders->contains($order)) {
+            $this->orders[] = $order;
+            $order->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrder(Order $order): self
+    {
+        if ($this->orders->removeElement($order)) {
+            // set the owning side to null (unless already changed)
+            if ($order->getOwner() === $this) {
+                $order->setOwner(null);
+            }
+        }
 
         return $this;
     }
---
+++ docs/10-create-first-order/083-comands-links.md
@@ -0,0 +1,15 @@
+### Создание сущности Order:
+
+`symfony console make:entity Order`
+
+### Создание формы EditOrderFormType:
+
+`symfony console make:form EditOrderFormType`
+
+### Сформировать миграции:
+
+`symfony console make:migration`
+
+### Выполнить миграции:
+
+`symfony console doctrine:migrations:migrate`

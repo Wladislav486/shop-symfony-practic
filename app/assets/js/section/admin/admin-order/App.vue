b5966fc4f222a450f9d03b4ab1d49d2fbf2933e7
vladgorbunov486 <vladgorbunov486@gmail.com>
2024-01-04 11:39:42 +0500

    108 Список продуктов заказа

--- app/assets/js/section/admin/admin-order/App.vue
+++ app/assets/js/section/admin/admin-order/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <div>
-    vue app {{ testProperty }}
+  <div class="table-additional-selection">
+    <hr/>
+    <OrderProductItem
+        v-for="(orderProduct, index) in staticStore.orderProducts"
+        :key="orderProduct.id"
+        :order-product="orderProduct"
+        :index="index"
+    />
+    <hr/>
   </div>
 </template>
 
 <script>
 import {mapState} from "vuex";
+import OrderProductItem from "./components/OrderProductItem.vue";
 
 export default {
-  created() {
-    console.log(window.staticStore.orderProducts)
-  },
+  components: {OrderProductItem},
   computed: {
-    ...mapState("products", ["testProperty"]),
-    productsCount() {
-      return 123;
-    }
+    ...mapState("products", ["staticStore"]),
   }
 }
 </script>
